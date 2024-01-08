ca423cae6f7fe2bcc84b1f7d4985a48b119fd986
Gaston Volpellier <gaston.volpellier@gmail.com>
2024-01-08 18:09:35 +0100

    Changes to 204, 209 and added notifications css

--- Scripts/204 Quickfilters/PTI-204 Create quickfilters on PLP.js
+++ Scripts/204 Quickfilters/PTI-204 Create quickfilters on PLP.js
@@ -14,7 +14,6 @@ const createElt = (tag, oAttr, content) => {
 };
 
 const init = () => {
-  console.log("DY | 204 quickfilters");
   const quickFiltersCtn = document.querySelector(".js-quick-filters-wrapper");
 
   if (quickFiltersCtn.firstElementChild) {
@@ -23,11 +22,10 @@ const init = () => {
       { class: "js-plp-quick-filters-btn fw-bold l-hmargin--large nowrap" },
       "${Button text}"
     );
+    stylesBtn.setAttribute("data-filter-id", "fit");
 
     quickFiltersCtn.firstElementChild.appendChild(stylesBtn);
   } else {
-    console.log("DY | not found ", count);
-
     if (count < 50 && !quickFiltersCtn.firstElementChild) {
       count++;
       setTimeout(init, 250);
--- Scripts/209 Change PLP description/PTI-209 Change PLP description.js
+++ Scripts/209 Change PLP description/PTI-209 Change PLP description.js
@@ -1,16 +1,13 @@
 let count = 0;
 
 const init = () => {
-  console.log("DY | 209 Replace description");
-
   const descriptionText = "${Description text}";
   const description = document.querySelector(".js-ellipsis-contents p");
 
   if (description) {
     description.innerHTML = descriptionText;
+    description.setAttribute("data-filter-id", "fit");
   } else {
-    console.log("DY | not found ", count);
-
     if (count < 50 && !description) {
       count++;
       setTimeout(replaceDescription, 250);
